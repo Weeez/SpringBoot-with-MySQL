94f48947bd224dffc474be851b6a12b7a2a66f29
Balint Babics <balintbabics@users.noreply.github.com>
2016-12-19 11:56:24 +0100

    Initial commit

---
+++ src/main/java/hu/bbabics/springboot_mysql/models/UserDao.java
@@ -0,0 +1,10 @@
+package hu.bbabics.springboot_mysql.models;
+
+import javax.transaction.Transactional;
+import org.springframework.data.repository.CrudRepository;
+
+@Transactional
+public interface UserDao extends CrudRepository<User, Long> {
+
+  public User findByEmail(String email);
+}
